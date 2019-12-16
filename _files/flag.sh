c696671a10ce614f0d4d1d1efc335234797400b5
Virink <virink@outlook.com>
2019-12-16 17:58:10 +0800

    Fuck git

--- _files/flag.sh
+++ _files/flag.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 
-<<<<<<< HEAD
-# Set dynamic FLAG
-# mysql -e "USE $FLAG_SCHEMA;ALTER TABLE FLAG_TABLE CHANGE FLAG_COLUMN $FLAG_COLUMN CHAR(128) NOT NULL DEFAULT 'not_flag';ALTER TABLE FLAG_TABLE RENAME $FLAG_TABLE;INSERT $FLAG_TABLE VALUES('$FLAG');" -uroot -proot
-# mysql -e "USE $FLAG_SCHEMA;UPDATE $FLAG_TABLE SET $FLAG_COLUMN=$FLAG" -uroot -proot
-
-
-# Reset mysql root's passwors as random string
-mysqladmin -uroot -proot password "toor$(date +%N)$RANDOM"
-=======
 # Create database and user
 # mysql -e "create database ctfhub;" -uroot -proot
-# mysql -e "grant select,insert,update,delete on ctfhub.* to ctfhub@'127.0.0.1' identified by 'ctfhub';" -uroot 
+# mysql -e "grant select,insert,update,delete on ctfhub.* to ctfhub@'127.0.0.1' identified by 'ctfhub';" -uroot
 
 # Set dynamic FLAG
 
@@ -26,7 +17,6 @@ mysqladmin -uroot -proot password "toor$(date +%N)$RANDOM"
 # Reset mysql root's passwors as random string
 PASS="toor$(date +%N)$RANDOM"
 mysqladmin -uroot -proot password $PASS
->>>>>>> 5e284c2... Update Base Image and Optimize
 
 export FLAG=not_flag
 FLAG=not_flag
